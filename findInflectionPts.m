b1666bc2a69071448963592ec283365cad3d69c9
jessi <jessi@DESKTOP-JJADVHJ>
2023-01-11 21:35:25 -0800

    Added findInflectionPts to project

---
+++ findInflectionPts.m
@@ -0,0 +1,66 @@
+function inflectionpts = findInflectionPts(inflectionpts,direction,row,col,peak2,minPeakPromPeak2,numPeaks,locs2i)
+
+if strcmp(direction,'row') == true
+    for i = 1:row
+    
+        % Find layer edges using peaks in 2nd peak mag values
+        invertPeak2 = peak2(i,:).^-1-1;
+        invertPeak2(isinf(invertPeak2)) = 0;
+        [~,magLoc] = findpeaks(invertPeak2,'MinPeakProminence',minPeakPromPeak2);
+        
+        % Find layer edges using 2nd peak label changes
+        peakLoc = [];
+        
+        for k = 2:col
+            if numPeaks(i,k) >= 2
+                if locs2i(i,k) ~= locs2i(i,k-1)
+                    peakLoc = [peakLoc, k]; %#ok<AGROW>
+                end
+            end
+        end
+        
+        % Merge both methods
+        locLocs = magLoc;
+        for k = 1:length(peakLoc)
+            if min(abs(peakLoc(k)-magLoc))>0
+                locLocs = [locLocs, peakLoc(k)]; %#ok<AGROW> 
+            end
+        end
+        locLocs = sort(locLocs);
+        inflectionpts(i,locLocs) = 1;
+    
+    end
+
+elseif strcmp(direction,'col') == true
+    for j = 1:col
+    
+        % Find layer edges using peaks in 2nd peak mag values
+        invertPeak2 = peak2(:,j).^-1-1;
+        invertPeak2(isinf(invertPeak2)) = 0;
+        [~,magLoc] = findpeaks(invertPeak2,'MinPeakProminence',minPeakPromPeak2);
+        
+%         % Find layer edges using 2nd peak label changes
+%         peakLoc = [];
+%         
+%         for i = 2:row
+%             if numPeaks(i,j) >= 2
+%                 if locs2i(i,j) ~= locs2i(i-1,j)
+%                     peakLoc = [peakLoc, i]; %#ok<AGROW>
+%                 end
+%             end
+%         end
+        
+        % Merge both methods
+        locLocs = magLoc';
+%         for k = 1:length(peakLoc)
+%             if min(abs(peakLoc(k)-magLoc))>5
+%                 locLocs = [locLocs, peakLoc(k)]; %#ok<AGROW> 
+%             end
+%         end
+%         locLocs = sort(locLocs);
+        inflectionpts(locLocs,j) = 1;
+    
+    end
+end
+
+end
